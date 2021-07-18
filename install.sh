2a2b8e90e3fd0452d75f16ceee9f51459465f4e7
waxdred <59381490+waxdred@users.noreply.github.com>
2021-07-18 17:00:14 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 install(){
+    echo "Install syntax_hight"
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
     echo "install Tmux"
     brew install tmux
     echo "copy .tmux"
