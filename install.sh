bdeec5e9d340fc8ed388f3582d713026d128c130
waxdred <59381490+waxdred@users.noreply.github.com>
2021-07-18 19:29:40 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,10 +10,15 @@ install(){
     echo "copy .tmux.conf"
     cp $PWD/tmux.conf $HOME/.tmux.conf
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    echo "get old version tmux"
     wget https://github.com/tmux/tmux/releases/download/3.1c/tmux-3.1c.tar.gz
     tar xopf tmux-3.1c.tar.gz
-    rm -rf $HOME/.brew/Cellar/tmux/3.2
+    echo "remove new version"
+    rm -rf $HOME/.brew/Cellar/tmux/3.2a
+    echo "add old version"
     mv tmux-3.1c $HOME/.brew/Cellar/tmux/3.1c
+    echo "Currente Version"
+    tmux -V
     tmux source ~/.tmux.conf
     echo "Prefix + I, for install the plugins"
     echo "well done"
